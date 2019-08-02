0f7edd9c2116a9585be09b862bef4778f58e801c
yusukeoyamada <fa19940118@gmail.com>
2019-08-02 12:21:55 +0900

    componentsを導入

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Game from './Game';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Game />
+      </div>
+    );
+  }
+}
+
+export default App;
